e4d23ff979d814cdc8edc539f4fdcfc5d78a1630
kukaro <kukaro4515@gmail.com>
2020-07-14 10:07:19 +0900

    default docker file

---
+++ script/sh/exec.sh
@@ -0,0 +1,1 @@
+docker exec -it travel-diary-storage-server-project /bin/bash
---
+++ script/sh/init.sh
@@ -0,0 +1,2 @@
+docker build -t travel-diary-storage-server-project ../../project/
+docker-compose -f ../../project/docker-compose.yml up -d
